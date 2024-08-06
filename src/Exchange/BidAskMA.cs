86ade88477636baedf9451c9242b80ca79f94e89
DSUN <dsun@hanmail.net>
2024-08-06 16:58:36 +0900

    !BidAskAlarmMA.Candles.ContainsKey

--- src/Exchange/BidAskMA.cs
+++ src/Exchange/BidAskMA.cs
@@ -91,6 +91,9 @@ namespace MetaFrm.Stock.Exchange
 
                 this.StatusBidAskAlarmMA ??= BidAskAlarmMA.ReadStatusBidAskAlarmMA(this.SettingID, this.ExchangeID, (int)this.MinuteCandleType, this.Market, this.LeftMA7, this.RightMA30, this.RightMA60, this.StopLossRate, this.Rate) ?? new();
 
+                if (!BidAskAlarmMA.Candles.ContainsKey($"{this.Market}_{(int)this.MinuteCandleType}"))
+                    BidAskAlarmMA.Candles.Add($"{this.Market}_{(int)this.MinuteCandleType}", new(this.Market, this.User.Api.ExchangeID, (int)this.MinuteCandleType));
+
                 this.Candles ??= BidAskAlarmMA.Candles[$"{this.Market}_{(int)this.MinuteCandleType}"];
 
 
